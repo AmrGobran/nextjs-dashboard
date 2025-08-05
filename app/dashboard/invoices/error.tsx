02fbb55bafec99ed51361713edce5a6508619ce5
Amr Gobran <amrgobran0100@gmail.com>
2025-08-06 01:20:57 +0300

    finished

---
+++ app/dashboard/invoices/error.tsx
@@ -0,0 +1,26 @@
+'use client'; // error.tsx needs be client component
+
+import { useEffect } from 'react';
+
+type Props = {
+	error: Error & { digest?: string }; // An instance of JavaScript native Error object
+	reset: () => void; // try to re-render the route segment
+};
+
+export default function Error({ error, reset }: Props) {
+	useEffect(() => {
+		console.log(error);
+	}, [error]);
+
+	return (
+		<main className="flex h-full flex-col items-center justify-center">
+			<h2 className="text-center">Something went wrong!</h2>
+			<button
+				className="mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400"
+				onClick={() => reset()}
+			>
+				Try again
+			</button>
+		</main>
+	);
+}
